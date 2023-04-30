ee7ac744f61b520b3497ec7cf295d11230820ca6
Tj_ <tjvishwa88@gmail.com>
2023-04-30 20:38:30 +0530

    "out of CRUD Read part was established from mongodb Atlas"

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 const mongoURI = 'mongodb+srv://FoodPanda:XfcBAQX4d0WEd86T@cluster0.klyxuv6.mongodb.net/foodPandaMERN?retryWrites=true&w=majority';
 const mongoDB = async ()=>{
-    await mongoose.connect(mongoURI,{ useNewUrlParser: true });
+    await mongoose.connect(mongoURI,{useNewUrlParser: true},async (err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Connected");
+            const fetched_data = await mongoose.connection.db.collection("food_items");
+            fetched_data.find({}).toArray(function(err, data){
+                if(err) console.log(err);
+                else console.log(data);
+            })
+        }
+    });
 }
 
 
